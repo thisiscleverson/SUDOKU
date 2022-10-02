c1283391c32aecd2e71250857ca97b0d63af5cb6
cleverson <cleverson2020333@gmail.com>
2022-10-02 18:43:39 -0300

    foi adicionado a parte que verifica se existe N° repetidos.

--- script/index.js
+++ script/index.js
@@ -7,21 +7,40 @@ const sudokuGame = {
 
         let amountOfSquare = (height*width)/9; // define the amount of squares that must be generated
 
-        function checkSquare(board, square){
+        function checkSquare(board, row){
+
+            // Check if there are numbers recessed on the same line and column
             for(let i=0; i<board.length;i++){
                 for(r=0; r<9; r++){
-                    if(square[r] == board[i][r] && square[r] > 0){
+                    if(row[r] == board[i][r] && row[r] > 0){
                         return;
                     }
                 }
             }
 
-            board.push(square)
+            //verificar se em um quadrado tem elementos repetidos
+            if(board.length >= 2){
+
+                for(let i=0; i<3; i++){
+
+                    console.log(board[0].some(el => {
+                        if(board[1].slice(0,3).includes(el) && row.slice(0,3).includes(el) && el != '') {
+                            console.log('elemento: '+el)
+                            console.log('board 1: '+board[0])
+                            console.log('board 2: '+board[1])
+                            console.log('row: '+row)
+                            return;
+                          }
+                    }))
+                }
+            }
+
+            board.push(row)
         }
 
 
         function generateNumbers(){
-            let square = ['0','0','0','0','0','0','0','0','0']
+            let row = ['','','','','','','','','']
             let i = 0;
             let limit = Math.floor(Math.random() * (9 - 2) + 2) 
     
@@ -31,23 +50,22 @@ const sudokuGame = {
     
                 // Verify that the generated number is not in the table
                 // check also if the position where the generated number is free to be placed
-                if(!square.includes(`${element}`) && square[position] == '0'){
-                    square[position] = `${element}`
+                if(!row.includes(`${element}`) && row[position] == ''){
+                    row[position] = `${element}`
                     i++
                 }
             }
-    
-            return square;
+            return row;
         }
-    
+
 
         while(this.board.length < amountOfSquare){
-            const square = generateNumbers()
+            const row = generateNumbers()
             
             if(this.board.length === 0){
-                this.board.push(square)
+                this.board.push(row)
             }else{
-                checkSquare(this.board, square)
+                checkSquare(this.board, row)
             }
         }
 
@@ -73,7 +91,7 @@ const sudokuGame = {
 }
 
 sudokuGame.generateTable(6,6)
-console.log(sudokuGame.board)
+//console.log(sudokuGame.board)
 
 
 
